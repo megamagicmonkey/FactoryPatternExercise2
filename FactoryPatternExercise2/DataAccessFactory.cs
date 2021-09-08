fcac12a70130dbb4ced159b127eaf4f85f8fdeed
megamagicmonkey <megamagicmonkey@gmail.com>
2021-09-07 20:07:26 -0500

    Initial commit

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDataAccessType(string databaseType)
+        {
+            switch (databaseType)
+            {
+                case "list":
+                    return new ListDataAccess();
+                    
+                case "sql":
+                    return new SQLDataAccess();
+                    
+                case "mongo":
+                    return new MongoDataAccess();
+                    
+                default:
+                    Console.WriteLine("You should see this, but you're getting a list");
+                    return new ListDataAccess();
+                    
+
+            }
+        }
+    }
+}
---
+++ FactoryPatternExercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FactoryPatternExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            
+            Console.WriteLine("Which database do you wish to access?");
+            Console.WriteLine("1) [list] 2) [sql] 3) [mongo]");
+
+            input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "1":
+                case "list":
+
+                    var dataList = DataAccessFactory.GetDataAccessType("list");
+                    dataList.LoadData();
+                    dataList.SaveData();
+                    break;
+                case "2":
+                case "sql":
+
+                    dataList = DataAccessFactory.GetDataAccessType("sql");
+                    dataList.LoadData();
+                    dataList.SaveData();
+                    break;
+                case "3":
+                case "mongo":
+
+                    dataList = DataAccessFactory.GetDataAccessType("mongo");
+                    dataList.LoadData();
+                    dataList.SaveData();
+                    break;
+                default:
+                    Console.WriteLine("Were the instructions really that hard?");
+                    break;
+            }
+        }
+    }
+}
